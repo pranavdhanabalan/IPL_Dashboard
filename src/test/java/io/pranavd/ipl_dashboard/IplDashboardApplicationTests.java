84ab6a5bdd6d9d30c30185a69970fbb0ce6d1d07
pranavdhanabalan <dhanabalanpranav@gmail.com>
2026-06-04 02:09:40 +0530

    Initial commit

---
+++ src/test/java/io/pranavd/ipl_dashboard/IplDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package io.pranavd.ipl_dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IplDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
