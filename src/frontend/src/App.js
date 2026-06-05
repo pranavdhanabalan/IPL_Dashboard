4d48bb0b2dc0ae040c82ee5181e6c0dd7c818940
pranavdhanabalan <dhanabalanpranav@gmail.com>
2026-06-05 18:25:55 +0530

    React app

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import {TeamPage} from "./pages/TeamPage";
-import {} from
+import {BrowserRouter as BrowserRouter, Route, Routes} from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <TeamPage />
+        <BrowserRouter>
+            <Routes>
+                <Route path="/team/:teamName" element={<TeamPage />} />
+            </Routes>
+        </BrowserRouter>
     </div>
   );
 }
