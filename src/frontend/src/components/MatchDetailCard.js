a6c26178cfa3043457c5e03ebb87d6d0bf8cc86d
pranavdhanabalan <dhanabalanpranav@gmail.com>
2026-06-05 18:25:44 +0530

    React app

--- src/frontend/src/components/MatchDetailCard.js
+++ src/frontend/src/components/MatchDetailCard.js
@@ -1,14 +1,15 @@
 import {React} from 'react';
 
-export const MatchDetailCard=({match})=> {
+export const MatchDetailCard=({match,teamName})=> {
     if (!match) {
         return null;
     }
+    const otherTeam=match.team1===teamName?match.team2:match.team1;
     return (
         <div className="MatchDetailCard">
             <h3>Latest Matches</h3>
             <h4>Match Details</h4>
-            <h4>{match.team1} vs {match.team2}</h4>
+            <h4>vs {otherTeam}</h4>
         </div>
     );
 }
--- src/frontend/src/pages/TeamPage.js
+++ src/frontend/src/pages/TeamPage.js
@@ -1,14 +1,15 @@
 import {React, useEffect, useState} from 'react';
+import {useParams} from "react-router-dom";
 import {MatchDetailCard} from "../components/MatchDetailCard";
 import {MatchSmallCard} from "../components/MatchSmallCard";
 
 export const TeamPage=()=> {
     const [team,setTeam]=useState({matches: []});
-
+    const {teamName}=useParams();
     useEffect(
         ()=>{
             const fetchMatches=async ()=>{
-              const response=await fetch('http://localhost:8080/team/Delhi Capitals');
+              const response=await fetch(`http://localhost:8080/team/${teamName}`);
               const data=await response.json();
               console.log(data);
               setTeam(data);
@@ -22,7 +23,7 @@ export const TeamPage=()=> {
     return (
         <div className="TeamPage">
             <h1>{team.teamName}</h1>
-            <MatchDetailCard match={team.matches[0]}/>
+            <MatchDetailCard teamName={team.teamName} match={team.matches[0]}/>
             {team.matches.slice(1).map(match=><MatchSmallCard match={match} />)}
         </div>
     );
